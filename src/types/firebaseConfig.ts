d91a638545a5806255c7bfd998c783f3a7a9c066
AmanDevelops <amanpal2072004@gmail.com>
2025-06-05 19:27:16 +0530

    refactor: migrate firebase configuration to use environment variables

--- src/types/firebaseConfig.ts
+++ src/types/firebaseConfig.ts
@@ -3,12 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVJbW6rdpZQ7HFV9RnXRQKyIjjYNWthT0",
-  authDomain: "am-devs-testing.firebaseapp.com",
-  projectId: "am-devs-testing",
-  storageBucket: "am-devs-testing.firebasestorage.app",
-  messagingSenderId: "900247965169",
-  appId: "1:900247965169:web:520aa2d04ad43b4d044850",
+  apiKey: process.env.apiKey,
+  projectId: process.env.projectId,
 };
 
 const app = initializeApp(firebaseConfig);
