9dcc48dff8d59eebe7d01114e6b772ebedbc7710
AmanDevelops <amanpal2072004@gmail.com>
2025-06-05 19:45:48 +0530

    refactor: update firebase configuration to use import.meta.env for environment variables

--- src/types/firebaseConfig.ts
+++ src/types/firebaseConfig.ts
@@ -3,8 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  projectId: process.env.projectId,
+  apiKey: import.meta.env.VITE_API_KEY,
+  projectId: import.meta.env.VITE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
